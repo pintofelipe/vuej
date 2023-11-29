af32d0abba467c9b701aad4548928fbadfb126a9
pintofelipe <afpintou@ufpso.edu.co>
2023-11-29 16:50:32 -0500

    create the component

---
+++ creación de componentes/src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue';
+
+import TaskComponent from '@/components/TaskComponent.vue'
+
+
+let task = ref(['Estudiar para algebra','Descansar','Sacar 5.0','ir a natación','Estudiar fracciones parciales'])
+
+let deleteTask = (index) => task.value.splice(index,1)
+
+
+</script>
+
+<template>
+
+<div class=" bg-gray-200 w-96 shadow-md rounded mx-auto px-4 py-2 mt-12">
+  <h1 class="text-6xl text-center font-semibold" >
+    Coco77
+  </h1>
+<div class="flex mt-2">
+  <input class="outline-none border-purple-900 rounded-r-none rounded bg-purple-200 w-4/5 border py-1 px-2 h-8" placeholder="Escribe su tarea">
+
+  <span class=" transition-colors duration-300 hover:bg-purple-950 hover:cursor-pointer rounded-l-none rounded w-1/5 h-8 bg-purple-800 text-lg text-center font-bold text-white">+</span>
+</div>
+
+<TaskComponent
+v-for="(tarea,index) in task" 
+:key="index"
+:tarea = "tarea"
+:index = "index"
+@borrarTarea = "deleteTask"
+/>
+
+</div>
+
+
+</template>
---
+++ creación de componentes/src/components/TaskComponent.vue
@@ -0,0 +1,29 @@
+<script setup>
+const props = defineProps({
+  tarea: {
+    type: String,
+    default: 'tarea por defecto'
+  },
+  index: {
+    type: Number
+  }
+})
+
+let emit = defineEmits(['borrarHijo'])
+
+let borrarTarea = (index) => {
+  emit('borrarTarea', index)
+}
+
+</script>
+
+<template>
+  <div class="px-4 py-1 rounded mt-1 flex bg-purple-500 justify-between">
+    {{ props.tarea }}
+    <span
+      @click="borrarTarea(props.index)"
+      class="px-2 transition-colors duration-300 hover:bg-purple-950 hover:cursor-pointer rounded bg-purple-800 text-center text-white"
+      >X</span
+    >
+  </div>
+</template>
